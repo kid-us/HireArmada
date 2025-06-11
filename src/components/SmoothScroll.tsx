e7a7c0ec46f498085a0502512243043d3b9d4f58
kid-us <kidushh29@gmail.com>
2025-06-11 06:45:45 +0300

    Smooth scroll animation increased

--- src/components/SmoothScroll.tsx
+++ src/components/SmoothScroll.tsx
@@ -9,7 +9,7 @@ export default function SmoothScroll({
 }) {
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 3.5,
+      duration: 2,
       smoothWheel: true,
       easing: (t: number) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
     });
