d5a0ce98b600d026fa3d27d4f74800cd9c1e7f7c
kid-us <kidushh29@gmail.com>
2025-06-21 15:49:35 +0300

    Unused variable removed

--- src/components/LogoCarousel.tsx
+++ src/components/LogoCarousel.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import Image from "next/image";
 
